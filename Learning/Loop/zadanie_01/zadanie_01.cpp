27d0b78a8fbc6cb0cf385c9ed350894a253cc839
Szymon <szymonmozol123@gmail.com>
2024-05-11 16:01:41 +0200

    zad01
    Napisz program wyświetlający liczby całkowite z przedziału <0,y> (wartość y podaje użytkownik)

--- Learning/Loop/zadanie_01/zadanie_01.cpp
+++ Learning/Loop/zadanie_01/zadanie_01.cpp
@@ -1,20 +1,17 @@
-﻿// zadanie_01.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+// Napisz program wyświetlający liczby całkowite z przedziału <0,y> (wartość y podaje użytkownik)
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int x;
+	cout << "Podaj koniec przedzialu: ";
+	cin >> x;
+
+	for (int i = 0; i <= x; i++) {
+		cout << i << " ";
+	}
+	return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
