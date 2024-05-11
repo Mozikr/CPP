772996c11f10675fb87e5655046159ee2b9d990f
Szymon <szymonmozol123@gmail.com>
2024-05-11 16:22:43 +0200

    zad

---
+++ Learning/KursUnreal/zadanie_01/zadanie_01.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Napisz program, który oblicza silnię podanej przez użytkownika liczby całkowitej.
+
+int Silnia(int n)
+{
+
+    int sum = 1;
+
+    for (int i = 1; i <= n; ++i) {
+        sum *= i;
+    }
+    return sum;
+}
+
+int main()
+{
+    int x;
+    cout << "Podaj liczbe do stworzenia silni: ";
+    cin >> x;
+
+    cout << "Twoja silnia to: " << Silnia(x);
+
+    
+}
+
