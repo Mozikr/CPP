0c6ae9414b3e95f43c1a6e193d037ed0e5c53303
Szymon <szymonmozol123@gmail.com>
2024-05-11 19:40:48 +0200

    zadanie_04

---
+++ Learning/KursUnreal/zadanie_04/zadanie_04.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Napisz funkcję która w tablicy znakowej zamienia małe litery na wielkie i na odwrót
+
+void ToggleChar(char tab[], int size)
+{
+    int gap = 'a' - 'A';
+
+    for(int i = 0; i < size; ++i)
+    {
+        if(tab[i] >= 'a' && tab[i] <= 'z')
+        {
+            tab[i] -= gap;
+        }
+        else if(tab[i] >= 'A' && tab[i] <= 'Z'){
+            tab[i] += gap;
+        }
+    }
+
+
+}
+
+int main()
+{
+    char tab[] = "aaalFAbeTt";
+    
+    cout << "napis przed: " << tab << endl;
+
+    ToggleChar(tab, 10);
+
+    cout << "napis po: " << tab << endl;
+    return 0;
+}
+
