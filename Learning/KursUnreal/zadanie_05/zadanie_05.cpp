ebb732cf046d3473d2d4df2185e272e4fb6fb9cf
Szymon <szymonmozol123@gmail.com>
2024-05-11 19:50:19 +0200

    zadanie_05

---
+++ Learning/KursUnreal/zadanie_05/zadanie_05.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Napisz funkcję która obliczy sumę cyfr liczby całkowitej
+
+int SumOfDigits(int number)
+{
+    int result = 0;
+
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    cout << "Suma cyfr liczby: " << SumOfDigits(x);
+}
+
