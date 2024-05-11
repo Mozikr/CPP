e6b7d206c36ba1333e0716315762d4c536b57a23
Szymon <szymonmozol123@gmail.com>
2024-05-11 17:43:39 +0200

    zad3

--- Learning/KursUnreal/zadanie_03/zadanie_03.cpp
+++ Learning/KursUnreal/zadanie_03/zadanie_03.cpp
@@ -1,20 +1,29 @@
-﻿// zadanie_03.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+//Napisz funkcję która obliczy średnia arytmetyczną z wartości w tablicy typu int.
+
+int Srednia(int tab[], int size) 
+{
+    int sum = 0;
+
+    for (int i = 0; i < size; ++i)
+    {
+        sum += tab[i];
+    }
+    cout << "Suma: " << sum << endl;
+
+    return sum / size;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int tab[] = { 1, 2, 3, 4, 5 };
+
+    cout << "Srednia: " << Srednia(tab, 5);
+
+    return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
