fdf518e5868f232b1609373f3c25f73df174fbde
Szymon <szymonmozol123@gmail.com>
2024-05-11 17:15:41 +0200

    zadanie_02

--- Learning/KursUnreal/zadanie_02/zadanie_02.cpp
+++ Learning/KursUnreal/zadanie_02/zadanie_02.cpp
@@ -1,9 +1,31 @@
-﻿// zadanie_02.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+//Napisz funkcję, która znajdzie największy element w tablicy typu int.
+
+int MaxInt(int tab[], int size) 
+{
+    if (size < 1) {
+        return INT_MIN; 
+    }
+
+    int max = tab[0];
+
+    for (int i = 0; i < size; i++) {
+        if (max < tab[i]) {
+            max = tab[i];
+        }
+    }
+
+    return max;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int tab[] = { 1, 3, 4, 6, 21, 44, 23, 7, 10, 9, 17 };
+
+    cout << "Najwiekszy element: " << MaxInt(tab, 10);
+
+    return 0;
 }
