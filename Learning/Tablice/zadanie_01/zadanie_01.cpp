0555bf37c62c7d13c6cda90101441b29183b1cc3
Szymon <szymonmozol123@gmail.com>
2024-05-11 15:50:48 +0200

    gfd
    gfd

--- Learning/Tablice/zadanie_01/zadanie_01.cpp
+++ Learning/Tablice/zadanie_01/zadanie_01.cpp
@@ -1,4 +1,4 @@
-﻿// Learning.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+﻿// zadanie_01.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
 
 #include <iostream>
@@ -9,12 +9,12 @@ int main()
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowabcvbnie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
 // Porady dotyczące rozpoczynania pracy:
 //   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
 //   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
 //   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
 //   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wssybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
 //   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
