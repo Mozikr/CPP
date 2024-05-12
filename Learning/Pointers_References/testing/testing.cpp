a4f54ac1250b148e8e1b2b64e6a36f38def55136
Szymon <szymonmozol123@gmail.com>
2024-05-12 19:47:35 +0200

    pointers

--- Learning/Pointers_References/testing/testing.cpp
+++ Learning/Pointers_References/testing/testing.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main()
 {
-    
+    int i = 10;
+    int* wsk = &i;
+
+    cout << wsk; // wyświetli adres
+    cout << *wsk; // wyświetli 10
+
+    // & - operator adresu, zwraca adres zmiennej
+    // * - operator wysłuskania, zwraca zmienną, na którą
+    // wskazuje wskaźnik
 }
 
