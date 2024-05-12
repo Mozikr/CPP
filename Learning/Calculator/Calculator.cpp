1403076336e931013d0431844b4accefcfa2c611
Szymon <szymonmozol123@gmail.com>
2024-05-12 12:53:08 +0200

    calculator and tictactoe

---
+++ Learning/Calculator/Calculator.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Napisz kalkulator :D
+
+int main()
+{
+    while (true)
+    {
+        // kontrola uzytkownika
+        char i;
+        float x, y;
+        cout << "Podaj pierwsza liczbe: " << endl;
+        cin >> x; //pierwsza zmienna
+        cout << "Podaj druga liczbe: " << endl;
+        cin >> y; //druga zmienna
+
+        cout << "Wybierz dzialanie (+,-,*,/), 'x' by zakonczyc. " << endl;
+        cin >> i;
+        //polecenia
+        switch (i)
+        {
+        case '+':
+            cout << "Wybrano dodawanie" << endl;
+            cout << "Suma: " << x + y << endl;
+            break;
+
+        case '-':
+            cout << "Wybrano odejmowanie" << endl;
+            cout << "Roznica: " << x - y << endl;
+            break;
+
+        case '*':
+            cout << "Wybrano mnozenie" << endl;
+            cout << "Iloczyn: " << x * y << endl;
+            break;
+
+        case '/':
+            cout << "Wybrano dzielenie" << endl;
+            cout << "Iloraz: " << x / y << endl;
+            break;
+
+        case 'x':
+        case 'X':
+            return 0;
+
+        default:
+            cout << "Podano niepoprawny symbol" << endl;
+            break;
+        }
+    }
+}
---
+++ Learning/TicTacToe/TicTacToe.cpp
@@ -0,0 +1,171 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+#define BOARD_SIZE 3
+#define BLANK_SPACE '_'
+char BOARD[BOARD_SIZE][BOARD_SIZE];
+
+void Initialize()
+{
+	for (int i = 0; i < BOARD_SIZE; ++i)
+	{
+		for (int j = 0; j < BOARD_SIZE; ++j)
+		{
+			BOARD[i][j] = BLANK_SPACE;
+		}
+	}
+}
+
+void Render()
+{
+	system("cls");
+
+	cout << "  ";
+	for (int i = 0; i < BOARD_SIZE; ++i)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < BOARD_SIZE; ++i)
+	{
+		cout << i << ' ';
+		for (int j = 0; j < BOARD_SIZE; ++j)
+		{
+			cout << BOARD[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+bool HasWon(char playerSymbol, int x, int y)
+{
+	bool result = true;
+
+	// wiersze
+	for (int i = 0; i < BOARD_SIZE; ++i)
+	{
+		if (BOARD[x][i] != playerSymbol)
+		{
+			result = false;
+			break;
+		}
+	}
+
+	if (result) return true;
+
+	result = true;
+
+	// kolumny
+	for (int i = 0; i < BOARD_SIZE; ++i)
+	{
+		if (BOARD[i][y] != playerSymbol)
+		{
+			result = false;
+			break;
+		}
+	}
+
+	if (result) return true;
+
+	result = true;
+
+	// skosy
+	if (x == y || x + y == BOARD_SIZE - 1)
+	{
+		result = true;
+		for (int i = 0; i < BOARD_SIZE; ++i)
+		{
+			if (BOARD[i][i] != playerSymbol)
+			{
+				result = false;
+				break;
+			}
+		}
+		if (result) return true;
+		result = true;
+		for (int i = 0; i < BOARD_SIZE; ++i)
+		{
+			if (BOARD[i][BOARD_SIZE - 1 - i] != playerSymbol)
+			{
+				result = false;
+				break;
+			}
+		}
+		if (result) return true;
+	}
+
+	return false;
+}
+
+
+bool PlayerInput(char playerSymbol)
+{
+	int x, y;
+	cout << "Podaj wiersz: ";
+	cin >> x;
+	cout << "Podaj kolumne: ";
+	cin >> y;
+
+	if (BOARD[x][y] == BLANK_SPACE)
+	{
+		BOARD[x][y] = playerSymbol;
+		return HasWon(playerSymbol, x, y);
+	}
+	else
+	{
+		cout << "To pole jest zajete" << endl;
+		PlayerInput(playerSymbol);
+	}
+}
+
+bool BotPlayer()
+{
+	while (true)
+	{
+		int x = rand() % BOARD_SIZE;
+		int y = rand() % BOARD_SIZE;
+
+		if (BOARD[x][y] == BLANK_SPACE)
+		{
+			BOARD[x][y] = 'O';
+			return HasWon('O', x, y);
+		}
+	}
+}
+
+int main()
+{
+	srand(time(nullptr));
+
+	Initialize();
+
+	for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; ++i)
+	{
+		Render();
+		if (i % 2 == 0)
+		{
+			if (PlayerInput('X'))
+			{
+				Render();
+				cout << "Wygrales, gratulacje!";
+				return 0;
+			}
+		}
+		else
+		{
+			if (BotPlayer())
+			{
+				Render();
+				cout << "Przegrales, wygral BOT!";
+				return 0;
+			}
+		}
+	}
+	cout << "Remis!" << endl;
+
+
+	return 0;
+}
