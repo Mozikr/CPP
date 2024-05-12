dc9cd262f179dc1ea0cf59a3d8fde3d92b4e3d48
Szymon <szymonmozol123@gmail.com>
2024-05-12 11:58:37 +0200

    zadanie_06

--- Learning/KursUnreal/zadanie_06/zadanie_06.cpp
+++ Learning/KursUnreal/zadanie_06/zadanie_06.cpp
@@ -4,7 +4,29 @@ using namespace std;
 
 //Napisz funkcje która sprawdzi czy dana tablica znaków jest palindromem.
 
+bool isPalindrom(char tab[])
+{
+    int size = strlen(tab);
+
+    int range = size / 2;
+
+    for(int i =0; i < range; ++i)
+    {
+        if (tab[i] != tab[size - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    char tab[255];
+    cin >> tab;
+
+    cout << "IsPalindrom: " << isPalindrom(tab);
+
+    return 0;
 }
