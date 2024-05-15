a28558c6d430a1150650f8ccc7fba0bd32da6a67
Szymon Mozol <szymonmozol123@gmail.com>
2024-05-16 00:08:57 +0200

    stos update

--- Learning/Pointers_References/stos_sterta/stos_sterta.cpp
+++ Learning/Pointers_References/stos_sterta/stos_sterta.cpp
@@ -3,5 +3,35 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
-}
+    // new zawsze zwraca adres czyli wsk
+    // raw pointer, smart pointery
+
+    int k = 10; //alokowanie na stosie - statyczne
+    int* d = new int; // alokowanie na stercie - dynamiczne
+
+    delete d; // zwolnienie pamieci zmiennej d
+
+    // memory leak
+
+    int size;
+    cout << "Podaj wielkosc tablicy wskaznikow: ";
+    cin >> size;
+
+    int** tab = new int* [size];
+
+    for (int i = 0; i < size; ++i)
+    {
+        tab[i] = new int[size];
+    }
+
+    tab[0][0] = 21;
+    cout << tab[0][0];
+
+    for (int i = 0; i < size; ++i)
+    {
+        delete[] tab[i];
+    }delete[] tab;
+}// zwolnienie pamieci zmiennej k
+
+
+
