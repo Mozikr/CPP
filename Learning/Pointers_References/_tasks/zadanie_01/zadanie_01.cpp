f83cd13148434874640f6388d10938882cdedb97
Szymon <szymonmozol123@gmail.com>
2024-05-23 21:28:56 +0200

    Pointers_cleaning_fixed

--- Learning/Pointers_References/_tasks/zadanie_01/zadanie_01.cpp
+++ Learning/Pointers_References/_tasks/zadanie_01/zadanie_01.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 // Napisz funkcję która przyjmuje dwie zmienne typu int przez wskaznik i zamienia je miejscami.
-void MySwapPointers(int* a, int*b) //wkaznik
+void MySwapPointers(int* a, int* b) //wkaznik
 {
-	if (a && b) 
+	if (a && b)
 	{
 		int temp = *a; //operujemy na adresach, wyłuskując mamy dostep do oryginalnych zmiennych bo mamy ich adresy
 		*a = *b;	//wyłuskiwanie
@@ -14,7 +14,7 @@ void MySwapPointers(int* a, int*b) //wkaznik
 
 void MySwapRef(int& a, int& b)
 {
-	if (a && b) 
+	if (a && b)
 	{
 		int temp = a;
 		a = b;
--- Learning/Pointers_References/_tasks/zadanie_02/zadanie_02.cpp
+++ Learning/Pointers_References/_tasks/zadanie_02/zadanie_02.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+//Napisz funkcję która przyjmuje przez wskaźnik tablicę typu int i dodaje do każdego elementu danę liczbę.
+
+void AddToArray(int* tab, int size, int number)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        tab[i] += number;
+
+    }
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    int TAB[5] = { 1,2,3,4,5 };
+
+    AddToArray(TAB, 5, 2);
+
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << TAB[i] << " ";
+    }
+
+    return 0;
 }
