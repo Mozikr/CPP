9fae68db49fcae4db4ae5fc1071b2b166a9878f9
Szymon <szymonmozol123@gmail.com>
2024-05-24 10:57:55 +0200

    zad_04

--- Learning/Pointers_References/_tasks/zadanie_04/zadanie_04.cpp
+++ Learning/Pointers_References/_tasks/zadanie_04/zadanie_04.cpp
@@ -1,20 +1,43 @@
-﻿// zadanie_04.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+//Napisz funkcję która przyjmuje jako parametr tablicę typu int i zwróci za pomocą referencji 3 największe wartośc tej tablicy (WHAAAT)
+//referencje moga sluzyc do zwracania wiecej niz jeden element z funckji
+
+void ThreeBigest(int tab[], int size, int& max1, int& max2, int& max3)
+{
+    max1 = max2 = max3 = INT_MIN;
+
+    for (int i = 0; i < size; ++i)
+    {
+        const int& current = tab[i];
+
+        if (current > max1)
+        {
+            max3 = max2;
+            max2 = max1;
+            max1 = current;
+        }
+        else if (current > max2)
+        {
+            max3 = max2;
+            max2 = current;
+        }
+        else if (current > max3)
+        {
+            max3 = current;
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int TAB[10] = { 1, 2, 4, 6, 7, 9, 0, 22, 100, 76 };
+
+    int a, b, c;
+
+    ThreeBigest(TAB, 10, a, b, c);
+
+    cout << a << " " << b << " " << c;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
