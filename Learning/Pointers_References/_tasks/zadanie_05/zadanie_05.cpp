3fbb8cc3024e0427799857ab15d6a7dcd10c7d87
Szymon <szymonmozol123@gmail.com>
2024-05-24 11:33:56 +0200

    zad_05

---
+++ Learning/Pointers_References/_tasks/zadanie_05/zadanie_05.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+//Napisz funkcję która tworzy dynamiczną tablicę jednowymiarową i wypełnia ją wartościami rosnącymi z danym krokiem
+
+int main()
+{
+    
+}
