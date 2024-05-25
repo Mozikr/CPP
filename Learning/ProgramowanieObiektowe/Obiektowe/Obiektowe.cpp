3ea1afaae032896a439f2532871387d7114241d1
Szymon <szymonmozol123@gmail.com>
2024-05-25 20:36:18 +0200

    obiektowe

--- Learning/ProgramowanieObiektowe/Obiektowe/Obiektowe.cpp
+++ Learning/ProgramowanieObiektowe/Obiektowe/Obiektowe.cpp
@@ -4,18 +4,50 @@ using namespace std;
 //deklaracja klasy
 class Car //nazwa klasy
 {
+
 public: //modyfikator dostepu
-    void Drive()//metoda
-    {
-        cout << "Brum Brum" << endl;
-    }
-    float speed = 10;//pole
+	Car() //kontruktor
+	{
+		speed = 0;
+		cout << "Car is created" << endl;
+	}
+
+	Car(float s) //kontruktor inicjalizujący
+	{
+		speed = s;
+		cout << "Car is created with speed" << endl;
+	}
+
+	~Car()//destruktor
+	{
+		cout << "Car is destroyed" << endl;
+	}
+
+
+	void Drive()//metoda
+	{
+		cout << "Brum Brum" << endl;
+	}
+
+	float GetSpeed() //getter, pobiera wartości prywatne
+	{
+		return speed;
+	}
+
+	void
+		SetSpeed(float s) //setter, ustawia wartości prywatne
+	{
+		speed = s;
+	}
+
+private:
+	float speed = 10;//pole
 };
 
 int main()
 {
-    Car car; //utworzenie instancji klasy(obiektu)
-    car.Drive();//wywołanie metody klasy
-    car.speed = 21;//odwołanie się do pola klasy
-    cout << car.speed << endl;
+	Car car; //utworzenie instancji klasy(obiektu)
+	car.Drive();//wywołanie metody klasy
+	car.SetSpeed(20);//odwołanie się do pola klasy
+	cout << car.GetSpeed() << endl;
 }
