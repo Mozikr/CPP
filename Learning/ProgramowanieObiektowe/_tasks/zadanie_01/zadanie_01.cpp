2d72786132045509661431709d50fd1b029047af
Szymon <szymonmozol123@gmail.com>
2024-05-26 12:20:40 +0200

    zadanie_01

---
+++ Learning/ProgramowanieObiektowe/_tasks/zadanie_01/zadanie_01.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+//Stworzyc klasę Car z polem speed typu int i metodami Drive(), Stop(), Accelerate().
+
+class Car
+{
+public: 
+
+    void Drive()
+    {
+        cout << "Brum Brum" << endl;
+    }
+
+    void Stop()
+    {
+        speed = 0;
+        cout << "Stop" << endl;
+    }
+
+    void Accelerate(float a)
+    {
+        speed += a;
+        cout << "Accelerate by: " << a << endl;
+    }
+
+private:
+    float speed = 0;
+};
+
+int main()
+{
+    Car car;
+
+    car.Accelerate(15);
+    car.Drive();
+    car.Stop();
+}
