46140f7a06fcc53e7abf16e5bcf655b5355af594
Szymon <szymonmozol123@gmail.com>
2024-05-26 20:04:41 +0200

    obiektowe 2

--- Learning/ProgramowanieObiektowe/Obiektowe2/Obiektowe2.cpp
+++ Learning/ProgramowanieObiektowe/Obiektowe2/Obiektowe2.cpp
@@ -16,11 +16,40 @@ public:
 
 protected:
     int m_age;
-}
+};
 
+
+class Student : public Person
+{
+public:
+
+    Student(int age, int indexNumber)
+    {
+        m_age = age;
+        m_indexNumber = indexNumber;
+    }
+
+
+
+    int GetIndexNumber()
+    {
+        return m_indexNumber;
+    }
+
+
+
+protected:
+    int m_indexNumber;
+};
 
 int main()
 {
-    
+    Student* student = new Student(20, 9001);
+    /*Deklaruje wskaźnik student typu Student* .
+    Dynamicznie alokuje pamięć dla obiektu Student na stercie.
+    Inicjalizuje obiekt Student za pomocą konstruktora z argumentami(20, 2121).
+    Przypisuje adres nowo utworzonego obiektu Student do wskaźnika student.*/
+
+    cout << "Age: " << student->GetAge() << endl << "Index number: " << student->GetIndexNumber();
 }
 
