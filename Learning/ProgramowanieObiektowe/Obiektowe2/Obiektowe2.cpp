40a66a57bba3a07fc5dae2c41c1574a9dacd2b82
Szymon <szymonmozol123@gmail.com>
2024-05-26 19:18:43 +0200

    Obiektowe 2

---
+++ Learning/ProgramowanieObiektowe/Obiektowe2/Obiektowe2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+    Person()
+    {
+
+    }
+
+    int GetAge()
+    {
+        return m_age;
+    }
+
+protected:
+    int m_age;
+}
+
+
+int main()
+{
+    
+}
+
