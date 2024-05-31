7e25beb3b8e15dcbdb25e3f6b6a6c11ace6a8281
Szymon <szymonmozol123@gmail.com>
2024-05-31 13:18:50 +0200

    namespace

--- Learning/Szablony/Szablony/Szablony.cpp
+++ Learning/Szablony/Szablony/Szablony.cpp
@@ -1,7 +1,20 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
+
+namespace myprogram
 {
-    
+    template<typename T>
+    T max(T a, T b)
+    {
+        return (a > b) ? a : b;
+    }
+
+
+    int main()
+    {
+        cout << myprogram::max(4, 5);
+
+        return 0;
+    }
 }
