0fbe472e80c65d5fc4f42354e0421ace5e39d10c
Szymon <szymonmozol123@gmail.com>
2024-05-31 07:35:05 +0200

    enum/struktury

---
+++ Learning/Struktury/Struktury/Struktury.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Point
+{
+	float x = 0;
+	float y = 0;
+};
+
+Point MovePoint(Point point, Point vector)
+{
+	Point result;
+	result.x = point.x + vector.x;
+	result.y = point.y + vector.y;
+	return result;
+}
+
+enum Animal
+{
+	Cat,
+	Dog,
+	Lion,
+};
+
+int main()
+{
+	/*Point p;
+	p.x = 10.2;
+	p.y = 21.2;
+
+	Point result = MovePoint(p, Point{ 5,5 });
+
+	cout << result.x << " " << result.y << endl;*/
+	return 0;
+}
