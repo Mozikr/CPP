c14a16ab0d114429e632fde3df969465068a202b
Szymon <szymonmozol123@gmail.com>
2024-06-01 12:36:14 +0200

    zadanie_01

--- Learning/Szablony/Szablony/Szablony.cpp
+++ Learning/Szablony/Szablony/Szablony.cpp
@@ -8,12 +8,54 @@ namespace my
 	{
 		return (a > b) ? a : b;
 	}
+
+	template<typename T>
+	class Container
+	{
+	public:
+		T array[10];
+		int size = 10;
+	};
+
+	template<>
+	class Container<float>
+	{
+	public:
+		float array[20];
+		int size = 20;
+	};
+
 }
 
 
 int main()
 {
-	cout << my::max(4, 5);
+	cout << my::max(4, 5) << endl;
 
+	my::Container<int> pojemnik;
+
+	for (int i = 0; i < pojemnik.size; ++i)
+	{
+		pojemnik.array[i] = i;
+		cout << pojemnik.array[i] << " ";
+	}
+	
+	cout << endl;
+	my::Container<char> pojemnik_char;
+
+	for (int i = 0; i < pojemnik_char.size; ++i)
+	{
+		pojemnik_char.array[i] = 'a' + i;
+		cout << pojemnik_char.array[i] << " ";
+	}
+
+	cout << endl;
+	my::Container<float> pojemnik_float;
+
+	for (int i = 0; i < pojemnik_float.size; ++i)
+	{
+		pojemnik_float.array[i] = i + 0.3f;
+		cout << pojemnik_float.array[i] << " ";
+	}
 	return 0;
 }
---
+++ Learning/Szablony/_tasks/zadanie_01/zadanie_01.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+//Napisz funkcję szablonową max, która przyjmuje dwa argumenty dowolnego typu i zwraca większy z nich.
+
+template<typename T>
+T maxNumber(T a, T b)
+{
+    return (a > b) ? a : b;
+}
+
+int main()
+{
+    int a, b;
+    cout << "Podaj pierwszy argument funkcji: " << endl;
+    cin >> a;
+    system("cls");
+    cout << "Podaj drugi argument funkcji: " << endl;
+    cin >> b;
+    system("cls");
+    cout << maxNumber(a,b);
+}
