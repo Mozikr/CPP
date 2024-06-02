379b48deb812da3160ddb85ff7f4e1369a5bc82a
Szymon <szymonmozol123@gmail.com>
2024-06-02 12:36:16 +0200

    vectory

--- Learning/BibliotekaStandardowaC++/BibliotekaStandardowa/BibliotekaStandardowa.cpp
+++ Learning/BibliotekaStandardowaC++/BibliotekaStandardowa/BibliotekaStandardowa.cpp
@@ -1,21 +1,55 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
+
+
+//lepiej przekazywac stringi poprzez referencje, unikamy wtedy kopiowania. A string to tablica wiec
+//musiała by się cała kopiować. Optymalizujemy to. A jeśli nie chcemy go modyfikować
+//to możemy go oznaczyć poprzez const - > const string& s
+//void Fun(string& s)
+//{
+//
+//}
 
 int main()
 {
-    string str = "hello world";
-    cout << str << endl;
-    cout << "Dlugosc: " << str.length() << endl;
-    string str2 = " how is your day?";
-    str += str2;
-    cout << str << endl;
-    string substr = str.substr(0, 5);
-    cout << substr << endl;
+    //string str = "hello world";
+    //cout << str << endl;
+    //cout << "Dlugosc: " << str.length() << endl;
+    //string str2 = " how is your day?";
+    //str += str2;
+    //cout << str << endl;
+    //string substr = str.substr(0, 5);
+    //cout << substr << endl;
+    //
+    //if (string::npos == str.find("hello")) //npos komunikuje ze nie znaleziono czegos
+    //{
+    //    cout << "Not found" << endl;
+    //}
+    //else cout << "Found it" << endl;
+
+    vector<int> numbers; //= { 1,2,3,4,5,6,7,8,9,10 };
+    numbers.reserve(100); //jak wiemy ile bedzie elementow to od razu ustawi capacity na 100
     
-    if (string::npos == str.find("hello")) //npos komunikuje ze nie znaleziono czegos
+    for (int i = 0; i < 100; ++i) //i<number.size()
     {
-        cout << "Not found" << endl;
+        numbers.push_back(i);
+        cout << numbers[i] << " ";
     }
-    else cout << "Found it" << endl;
+
+    //iterator struktura do przechodzenia po kolekcji
+    vector<int>::iterator it = numbers.begin(); //begin początek, rbegin od tyłu
+    while (it != numbers.end()) //sprawdzamy czy iterator nie doszedl do end
+    {
+        cout << *it << endl;
+        ++it;
+    }
+
+    cout << endl;
+    cout << numbers.size() << " " << numbers.capacity() << endl; //size - elementy, 
+    //capacity - Zwraca maksymalną liczbę elementów jaką 
+    //może pomieścić kontener bez wykonywania realokacji pamięci.
+    numbers.shrink_to_fit(); // zmniejsza jego capacity(maksymalną liczbę elementów)
+    //przydatne jak wiemy ze vector nie bedzie zmienial swoich wielkosci
 }
