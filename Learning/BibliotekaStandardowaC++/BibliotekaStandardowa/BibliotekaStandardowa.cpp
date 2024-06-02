deafbc7e9bcc59f20c9b0ad8dc1dc5331137a869
Szymon <szymonmozol123@gmail.com>
2024-06-02 11:24:08 +0200

    .

--- Learning/BibliotekaStandardowaC++/BibliotekaStandardowa/BibliotekaStandardowa.cpp
+++ Learning/BibliotekaStandardowaC++/BibliotekaStandardowa/BibliotekaStandardowa.cpp
@@ -5,5 +5,17 @@ using namespace std;
 int main()
 {
     string str = "hello world";
-    cout << str;
+    cout << str << endl;
+    cout << "Dlugosc: " << str.length() << endl;
+    string str2 = " how is your day?";
+    str += str2;
+    cout << str << endl;
+    string substr = str.substr(0, 5);
+    cout << substr << endl;
+    
+    if (string::npos == str.find("hello")) //npos komunikuje ze nie znaleziono czegos
+    {
+        cout << "Not found" << endl;
+    }
+    else cout << "Found it" << endl;
 }
