c1af596ba6a834e5aae3ed3e33d994a6ef682ef3
Szymon <szymonmozol123@gmail.com>
2024-06-06 12:04:19 +0200

    BubbleSort

--- Learning/Podstawowe algorytmy/PodstawoweAlgorytmy/PodstawoweAlgorytmy.cpp
+++ Learning/Podstawowe algorytmy/PodstawoweAlgorytmy/PodstawoweAlgorytmy.cpp
@@ -28,9 +28,42 @@ int BinarySearch(const vector<int>& tab, int target, int right, int left = 0) //
     }
 }
 
+void BubbleSort(vector<int>& tab)
+{
+    for (int j = 0; j < tab.size(); ++j) 
+    {
+        for (int i = 0; i < tab.size()-1; ++i)
+        {
+            if (tab[i] > tab[i + 1])
+            {
+                swap(tab[i], tab[i + 1]);
+            }
+
+        }
+    }
+    
+}
+
 int main()
 {
-    vector<int> TAB = { 1,2,3,4,5,5,7,9,12,16,21,27,90 };
+    //vector<int> TAB = { 1,2,3,4,5,5,7,9,12,16,21,27,90 };
+    //cout << BinarySearch(TAB,100, TAB.size());
 
-    cout << BinarySearch(TAB,100, TAB.size());
+    vector<int> TAB;
+    const int size = 10;
+    TAB.reserve(size);
+    TAB.resize(size);
+    srand(time(NULL));
+
+    for (int i = 0; i < size; ++i)
+    {
+        TAB[i] = rand() % 100;
+    }
+
+    BubbleSort(TAB);
+
+    for (int i = 0; i < size; ++i)
+    {
+        cout << TAB[i] << ", ";
+    }
 }
